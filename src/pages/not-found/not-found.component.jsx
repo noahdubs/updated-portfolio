708daf329b6ad33c8d3c0c0bbd2e245929798290
merchencer <merchencer@gmail.com>
2021-10-01 23:28:42 -0500

    try again 2

--- src/pages/not-found/not-found.component.jsx
+++ src/pages/not-found/not-found.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react' 
+import { Link } from 'react-router-dom'
 
 import './not-found.styles.css'
 
@@ -6,7 +7,9 @@ const NotFound = () => (
     <div className="not-found">
         <h1 className="four-o-four">404</h1>
         <p className="not-found-p">Oops! Something is wrong.</p>
-        <a className="not-found-button" href="/"><i className="icon-home"></i> Go back to home page</a>
+        <Link to="/" className="not-found-button">
+            <i className="icon-home"></i> Go back to home page
+        </Link>
     </div>
 )
 
