acff0455b07ce0e28ecf340bf2c2312a5b722245
merchencer <merchencer@gmail.com>
2021-10-02 17:23:27 -0500

    add amplify

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
+import Amplify from "aws-amplify"
+import awsExports from "./aws-exports"
+Amplify.configure(awsExports)
+
+import './index.css'
+import App from './App'
 
 ReactDOM.render(
     <BrowserRouter>
