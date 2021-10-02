e5be5edfb92020be02e4f8614d9eafd24fed807f
merchencer <merchencer@gmail.com>
2021-10-01 22:38:17 -0500

    test 404

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import './app.css'
 import {Switch, Route, Redirect} from 'react-router-dom'
 
 import Homepage from './pages/homepage/homepage.component'
-import NotFound from './pages/not-found/not-found.component'
+// import NotFound from './pages/not-found/not-found.component'
 
 const App = () => {
 	return (
     	<div className="app">
 			<Switch>
 				<Route exact path="/" component={Homepage} />      
-
-				<Route path="/404" component={NotFound} />
-                <Redirect to="/404" />
+				{/* <Route path="/404" component={NotFound} />
+                <Redirect to="/404" /> */}
 			</Switch>
         	
     	</div>
