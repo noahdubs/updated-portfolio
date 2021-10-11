bd727c262b616097a8654dd4a4e853c988fdee5e
merchencer <merchencer@gmail.com>
2021-10-11 03:17:02 -0500

    removed amplify

--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
-import Amplify from "aws-amplify"
-import awsExports from "./aws-exports"
-
 import './index.css'
 import App from './App'
 
-Amplify.configure(awsExports)
 
 ReactDOM.render(
     <BrowserRouter>
         <App />
     </BrowserRouter>, 
-    document.getElementById('root'))
+    document.getElementById('root')
+)
 
